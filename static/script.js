7736addc784f5948714a8d8a5ba64d2d902f20ce
Met-Sam <117329674+Met-Sam@users.noreply.github.com>
2023-12-25 10:07:51 +0000

    gt v2

--- static/script.js
+++ static/script.js
@@ -1,30 +1,41 @@
-// Function to send a request to the Flask server to get the transcript
-function fetchTranscript() {
-    const url = document.getElementById('youtube-url').value;
+let storedURL = '';
+
+function fetchAndCopyTranscript() {
     const duration = document.getElementById('duration').value;
+    const captureBefore = document.getElementById('capture-before').checked;
 
-    fetch('/get_transcript', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ url, duration }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('transcript').textContent = data.transcript;
-    })
-    .catch(error => {
-        console.error('Error fetching transcript:', error);
-    });
-}
+    if (!duration) {
+        showNotification('Add Duration', true);
+        return;
+    }
 
-// Function to copy the transcript text to the clipboard
-function copyToClipboard() {
-    const transcriptText = document.getElementById('transcript').textContent;
-    navigator.clipboard.writeText(transcriptText).then(() => {
-        alert('Transcript copied to clipboard!');
+    // Use the stored URL if it exists; otherwise, read from the clipboard
+    const urlPromise = storedURL ? Promise.resolve(storedURL) : navigator.clipboard.readText();
+
+    urlPromise.then(url => {
+        // Check if the URL is different from the stored one
+        if (url !== storedURL) {
+            storedURL = url; // Update the stored URL if it's a new one
+        }
+        fetch('/get_transcript', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ url: storedURL, duration, captureBefore }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            const transcript = data.transcript || 'No transcript found.';
+            document.getElementById('transcript').value = transcript;
+            showNotification('Transcript Ready', false);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            showNotification('Error fetching transcript', true);
+        });
     }).catch(err => {
-        console.error('Could not copy text:', err);
+        console.error('Failed to read clipboard contents:', err);
+        showNotification('Error reading clipboard', true);
     });
 }
